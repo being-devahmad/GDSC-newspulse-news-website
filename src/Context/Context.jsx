9274ede369e868fb63969c176e8706ade151c966
being-devahmad <devahmad41@gmail.com>
2024-03-10 01:15:24 +0500

    axios commit

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -10,13 +11,17 @@ export const NewsProvider = ({ children }) => {
 
     // Fetch news data and update the state
     const getNewsData = async () => {
-        const response = await fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=87e3395bfeac484983350246ab27aa3e")
-        const data = await response.json()
-        const newsWithIds = data.articles.map(article => ({ ...article, id: uuidv4() }));
-        console.log(newsWithIds)
-        setNews(newsWithIds)
-        // console.log(data.articles);
-        // setNews(data.articles)
+        try {
+            const response = await axios("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=87e3395bfeac484983350246ab27aa3e")
+            const data = response.data
+            const newsWithIds = data.articles.map(article => ({ ...article, id: uuidv4() }));
+            console.log(newsWithIds)
+            setNews(newsWithIds)
+            // console.log(data.articles);
+            // setNews(data.articles)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     useEffect(() => {
